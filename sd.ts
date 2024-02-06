d9294929bfa427f986e9588e6750f8e28775f0d5
Joriks <gulev324@mail.ru>
2024-02-06 09:08:06 +0300

    update 06.02.24

---
+++ sd.ts
@@ -0,0 +1,27 @@
+// class Mybad{}
+// class Point{x:number; y:number}
+
+// function func() {
+//  let point:Point;
+// }
+
+// let point:Point = new Point();
+
+// function move(){
+//     point.x = 1; 
+//     point.y = 2;
+// }
+
+class Point3D{x:number; y:number; z:number}
+
+function buc(){
+    let point3d:Point3D;
+}
+
+let point3d:Point3D = new Point3D();
+
+function move3D(){
+    point3d.x = 1; 
+    point3d.y = 2;
+    point3d.z = 3;
+}
